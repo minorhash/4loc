2ab12bd291c45738e0ab5b8c9587c9587640eb5b
minorhash <minorhash@gmail.com>
2019-05-20 14:42:27 +0900

    05/20/19

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,24 @@
 var express = require('express');
 var router = express.Router();
 // glob
-var par
+var par,arr=[]
 
 var getPar=function(req, res, next) {
 
 par=req.params.id
 next()}
 
+var lsImg=function(req,res,next){
+var ls=require("ls")
+    var path="public/img/cd/png/*"
+
+    for (var i of ls(path)){
+console.log(i.name)
+        arr.push(i.name)
+
+}
+
+next()}
 
 var chk=function(req, res, next) {
 
@@ -18,13 +29,12 @@ next()}
 var gcb= function(req, res, next) {
 
 res.render("index", {
-title: par,
-par:par,
+title: par,par:par,arr:arr
 
 });
 }
 
-router.get('/', [getPar,chk,gcb])
+router.get('/', [getPar,lsImg,chk,gcb])
 // post
 
 
